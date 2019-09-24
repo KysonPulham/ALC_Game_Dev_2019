e807097f181f2c6b94ded08485b70ecc71ef5f6a
Kyson <kyson.p401@stu.nebo.edu>
2019-09-24 10:29:13 -0600

    Challenge 1
    Get donedid

---
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Player Control
+    private float speed = 30f;
+    private float turnSpeed = 15.0f;
+    private float horizontalInput;
+    private float fowardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move and Back
+        fowardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
